b1ddefa8dfe8faf316aeea44140afc8d11da2f6c
Khyati Gupta <khyatigupta21@gmail.com>
2021-10-09 10:33:02 +0800

    VSS KG changed reverse/join function with for loop in js file

--- assets/js/reverse-string.js
+++ assets/js/reverse-string.js
@@ -8,14 +8,21 @@ See test cases below.
 - 123 <=> 123
 */
 
-let str = "123ab45cd";
+let str = "1acb45xyz";
 const myArr = str.split(/(\d+)/); //split the string with regex
 let newStr = "";
 for (i = 0; i < myArr.length; i++) {
   if (myArr[i] !== "") {
     if (/\d/.test(myArr[i])) newStr += myArr[i];
     //test the array element with regex  whether it is number or not
-    else newStr += myArr[i].split("").reverse().join(""); // split the element and reverse
+    else {
+      let mySubArr = myArr[i].split("");
+      //console.log("line no 21", mySubArr);
+      for (j = mySubArr.length - 1; j >= 0; j--) {
+        newStr += mySubArr[j];
+      }
+      //newStr += myArr[i].split("").reverse().join(""); // split the element and reverse
+    }
   }
 }
 console.log("Reversed String is:", newStr); // print new string
