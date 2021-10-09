bbe15a7633d6e7e40171f2c31509b9c69423367f
Khyati Gupta <khyatigupta21@gmail.com>
2021-10-09 10:56:13 +0800

    created reverse function

--- assets/js/reverse-string.js
+++ assets/js/reverse-string.js
@@ -10,19 +10,26 @@ See test cases below.
 
 let str = "1acb45xyz";
 const myArr = str.split(/(\d+)/); //split the string with regex
-let newStr = "";
+let result = "";
+
+function reverseString(str) {
+  let newStr = "";
+  for (j = str.length - 1; j >= 0; j--) {
+    newStr += str[j];
+  }
+  return newStr;
+}
+
 for (i = 0; i < myArr.length; i++) {
   if (myArr[i] !== "") {
-    if (/\d/.test(myArr[i])) newStr += myArr[i];
+    if (/\d/.test(myArr[i])) result += myArr[i];
     //test the array element with regex  whether it is number or not
     else {
       let mySubArr = myArr[i].split("");
       //console.log("line no 21", mySubArr);
-      for (j = mySubArr.length - 1; j >= 0; j--) {
-        newStr += mySubArr[j];
-      }
-      //newStr += myArr[i].split("").reverse().join(""); // split the element and reverse
+      result += reverseString(mySubArr);
+      //result += myArr[i].split("").reverse().join(""); // split the element and reverse
     }
   }
 }
-console.log("Reversed String is:", newStr); // print new string
+console.log("Reversed String is:", result); // print new string
